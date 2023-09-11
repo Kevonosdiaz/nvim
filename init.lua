8104953aa851a2f18cb4eb772827baf51d2451f1
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-09-10 18:19:34 -0600

    Add Go to LSP and make theme transparent

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local lspconfig = require("lspconfig")
+lspconfig.gopls.setup({})
+
+vim.cmd([[autocmd BufWritePre *.go lua vim.lsp.buf.format({ async = true })]])
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+  end,
+})
