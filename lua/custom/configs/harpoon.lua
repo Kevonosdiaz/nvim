665144be9f420db598bbd66aead9cbc8475c7dee
Kevin Tran <Kevin.Tran@amd.com>
2025-02-27 14:34:32 -0700

    add ability to remove from harpoon

--- lua/custom/configs/harpoon.lua
+++ lua/custom/configs/harpoon.lua
@@ -5,6 +5,7 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Add to Harpoon" })
+vim.keymap.set("n", "<leader>y", function() harpoon:list():remove() end, { desc = "Remove from Harpoon" })
 -- vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end, { desc = "Select Harpoon 1" })
