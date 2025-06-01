441d794cb0a4ddf6355db64ceb8552e47e4e11f5
Kevin Tran <kevonosdiaz@gmail.com>
2025-06-01 00:43:57 -0600

    disable md preview for now

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,14 +10,14 @@ return {
       require 'custom.configs.lualine'
     end,
   },
-  {
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    ft = { 'markdown' },
-    build = function()
-      vim.fn['mkdp#util#install']()
-    end,
-  },
+  -- {
+  --   'iamcco/markdown-preview.nvim',
+  --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  --   ft = { 'markdown' },
+  --   build = function()
+  --     vim.fn['mkdp#util#install']()
+  --   end,
+  -- },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
