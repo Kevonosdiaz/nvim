88a5d37969287542e0b9c135f70d96c2212cd03b
Kevin Tran <kevonosdiaz@gmail.com>
2025-06-01 00:23:33 -0600

    mess around with lualine config a bit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,9 +7,7 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin/nvim' },
     config = function()
-      require('lualine').setup {
-        options = { theme = 'catppuccin' },
-      }
+      require 'custom.configs.lualine'
     end,
   },
   {
