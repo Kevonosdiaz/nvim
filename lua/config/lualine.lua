8a024e685d5a9319eed034ed024da98e37358a5f
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-08-03 10:07:51 -0600

    add everforest, cyberdream themes

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,11 +1,11 @@
-local lualine = require 'lualine'
+local lualine = require("lualine")
 
-lualine.setup {
+lualine.setup({
   options = {
     icons_enabled = true,
-    theme = 'catppuccin-mocha',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
     disabled_filetypes = {
       statusline = {},
       winbar = {},
@@ -21,22 +21,22 @@ lualine.setup {
     },
   },
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch' },
-    lualine_c = { 'filename' },
+    lualine_a = { "mode" },
+    lualine_b = { "branch" },
+    lualine_c = { "filename" },
     lualine_x = {
-      'progress',
+      "progress",
     },
     lualine_y = {
       "os.date('%-I:%-M')",
     },
-    lualine_z = { 'location' },
+    lualine_z = { "location" },
   },
   inactive_sections = {},
   tabline = {},
   winbar = {},
   inactive_winbar = {},
   extensions = {},
-}
+})
 
 vim.opt.laststatus = 3
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,22 +1,35 @@
 return {
   {
-    'rcarriga/nvim-notify',
+    "rcarriga/nvim-notify",
     config = function()
-      require('notify').setup {
-        background_color = '#181825',
-      }
-      vim.notify = require 'notify'
+      require("notify").setup({
+        background_color = "#181825",
+      })
+      vim.notify = require("notify")
     end,
   },
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    "sainnhe/everforest",
     priority = 1000,
-    config = function() vim.cmd [[colorscheme catppuccin-nvim]] end,
   },
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', 'catppuccin/nvim' },
-    config = function() require 'config.lualine' end,
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme catppuccin-nvim]])
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("config.lualine")
+    end,
   },
 }
