d6acebe226ae67b4123877af15c655948b9704ca
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2026-09-17 20:00:23 -0600

    add neorg

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -105,6 +105,12 @@ return {
     --   { "<localLeader>l", "", desc = "+vimtex" },
     -- },
   },
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    config = true,
+  },
   { -- Autoformat
     "stevearc/conform.nvim",
     event = { "BufWritePre" },
